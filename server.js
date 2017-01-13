9b580bed5feb8d08a103474449312bb7ceef5870
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-01-13 18:45:47 +0000

    finish first draft of project

--- server.js
+++ server.js
@@ -2,27 +2,52 @@ var express = require('express');
 var https = require('https');
 var request = require('request');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
-var options = {
-  url: 'https://api.imgur.com/3/gallery/search?q=lolcats',
-  method: 'GET',
-  headers: {
-    'Authorization': 'Client-ID 6c3bb20535ca5bd'
-  },
-};
+function requestObjectMaker(querystr) {
+  return {
+    url: 'https://api.imgur.com/3/gallery/search?q=' + querystr,
+    method: 'GET',
+    headers: {
+      'Authorization': 'Client-ID 6c3bb20535ca5bd'
+    },
+  }
+}
 
 app.get('/', function(req, res) {
   res.send('hello');
 });
 
 app.get('/test', function(req, res) {
-  request.get(options, function(err, data, body) {
+  request.get(requestObjectMaker('lolcats'), function(err, data, body) {
     res.end(body);
   });
 });
 
 app.get('/api/imagesearch/:query', function(req, res) {
-  res.end(req.params.query);
+  MongoClient.connect("mongodb://localhost:27017/data", function(err, db) {
+    if (err) throw err;
+    db.createCollection('recentSearches');
+    db.collection('recentSearches').save({ search: req.params.query});
+    db.close();
+  });
+  var offset;
+  var offset = parseInt(req.query.offset);
+  request.get(requestObjectMaker(req.params.query.split('?')[0]), function(err, data, body) { 
+ 						    if (offset) {
+						      res.end(JSON.stringify(JSON.parse(data.body).data.slice(0, offset)));
+						    } else { 
+                                                      res.end(JSON.stringify(JSON.parse(data.body).data)); 
+						    };
+ 						    })
 });
+
+app.get('/recent', function(req, res) {
+  MongoClient.connect("mongodb://localhost:27017/data", function(err, db) {
+    if (err) throw err;
+    db.collection('recentSearches').find({}, {'search':1, _id:0}, function(err, data) { data.toArray(function(err, data) { res.end(JSON.stringify(data)); }) });
+    db.close();
+  });
+})
 
 app.listen(8080);
