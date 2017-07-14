ea340b83a983606a41ec9d431c0baeb48486290f
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-07-14 16:27:21 +0000

    add connection to mlab db in test url

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient,
     api_url = 'https://api.imgur.com/3/gallery/search/?q=',
     clientid = '5cfe2aa5b1c8b48';
+
 const makeDbConnectUrl = () => {
   const username = 'louisheimel1'
   const password = 'superkey1'
   return 'mongodb://' + username + ':' + password + '@ds161099.mlab.com:61099/imagesearchabstraction'
 }
+
+const dbConnectUrl = makeDbConnectUrl();
 
 
 
@@ -43,24 +46,24 @@ app.get('/api/imagesearch/:query', function(req, res) {
     if (err) throw err;
     res.json(parseResponse(data))
   })
-//   MongoClient.connect("mongodb://localhost:27017/data", function(err, db) {
-//     if (err) throw err;
-//     db.createCollection('recentSearches');
-//     db.collection('recentSearches').save({ search: req.params.query});
-//     db.close();
-//   });
-//   var offset = parseInt(req.query.offset);
-//   request.get(requestObjectMaker(req.params.query.split('?')[0]), function(err, data, body) { 
-//  						    if (offset) {
-// 						      res.end(parseResponse(data).slice(0, offset));
-// 						    } else { 
-// 						      res.end(parseResponse(data))
-// 						    };
-//  						    })
+   MongoClient.connect(dbConnectUrl, function(err, db) {
+     if (err) throw err;
+     db.createCollection('recentSearches');
+     db.collection('recentSearches').save({ search: req.params.query});
+     db.close();
+   });
+   var offset = parseInt(req.query.offset);
+   request.get(requestObjectMaker(req.params.query.split('?')[0]), function(err, data, body) { 
+  						    if (offset) {
+ 						      res.end(parseResponse(data).slice(0, offset));
+ 						    } else { 
+ 						      res.end(parseResponse(data))
+ 						    };
+  						    })
 });
 
 app.get('/recent', function(req, res) {
-  MongoClient.connect(makeDbConnectUrl(), function(err, db) {
+  MongoClient.connect(dbConnectUrl, function(err, db) {
     if (err) throw err;
     db.collection('recentSearches').find({}, {'search':1, _id:0}, function(err, data) { data.toArray(function(err, data) { res.end(JSON.stringify(data)); }) });
     db.close();
