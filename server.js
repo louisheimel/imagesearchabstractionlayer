894dd291cf1b76202005014adfeb16e50bc99b96
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-07-14 18:15:30 +0000

    serve static assets

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ function parseResponse(data) {
   return JSON.parse(data.body).data.filter(e => !e.is_album);
 }
 
-app.use(express.static(public))
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
   res.send();
 });
